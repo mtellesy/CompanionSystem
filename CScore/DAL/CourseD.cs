79b78da49a96b4def0ed692bf085e1ac8a8f5ec4
mtellesy <m.al-tellesy@uot.edu.ly>
2016-11-27 21:10:30 +0200

    Adding classes to DAL
    Muhammad

--- CScore/DAL/CourseD.cs
+++ CScore/DAL/CourseD.cs
@@ -3,13 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CScore.DataLayer.Tables;
 using CScore.DataLayer;
+using CScore.BCL;
 using SQLite.Net;
 
 namespace CScore.DAL
 {
-    public class InboxD 
+   public static class CourseD
     {
-
     }
 }
