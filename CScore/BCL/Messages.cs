3b0ab5cbdf398f1cf4aac7af6dc28c78683b60d1
mtellesy <m.al-tellesy@uot.edu.ly>
2016-11-29 20:36:29 +0200

    Working on Messages

--- CScore/BCL/Messages.cs
+++ CScore/BCL/Messages.cs
@@ -8,5 +8,52 @@ namespace CScore.BCL
 {
     public class Messages
     {
+        public int mes_id { get; set; }
+
+        public int mes_status { get; set; }
+
+        public int mes_sender { get; set; }
+
+       // private String mes_subject { get; set; }
+
+      //  private String mes_time { get; set; }
+
+        public String mes_content { get; set; }
+
+        public static Messages getMesssage(int messageId)
+        {
+            Messages Message = new Messages();
+ 
+            // Message = await DAL.MessageD.getMessage(messageId);
+            return Message;
+        }
+
+        public static Boolean sendMessage(Messages Message)
+        {
+            if (UpdateBox.CheckForInternetConnection() == true)
+            {
+                
+                //SAL stuff
+                // save message in dal
+                return true;
+            }
+            else
+                return false;
+        }
+
+        public static List<Messages> getMessages(int NumberOfMessages, int startFrom,String type)
+        {
+            if (UpdateBox.CheckForInternetConnection() == true)
+            {
+
+                //SAL stuff
+                // save messages in dal
+               
+            }
+            //get messages from dal
+            List<Messages> tt = new List<Messages>();
+            return tt ;
+        }
+
     }
 }
--- CScore/DAL/MessageD.cs
+++ CScore/DAL/MessageD.cs
@@ -12,6 +12,47 @@ namespace CScore.DAL
 {
     public static class MessageD 
     {
+        public static async Task<List<Messages>> getSentMessages(int NumberOfMessages, int StartFrom)
+        {
+            var results = await  DBuilder._connection.Table<InboxL>().Where(i => i.Mes_sender.Equals(User.use_id)).OrderByDescending(i => i.Mes_id).ToListAsync();
+            // how limit and start from
+        // var resul =  await DBuilder._connection.QueryAsync<InboxL>(
+           //    "SELECT * FROM InboxL Where Mes_sender = ? Limit ?",User.use_id,NumberOfMessages
+         //      );
+         
+            List<Messages> messages = new List<Messages>();
 
+            foreach(var mesg in results)
+            {
+                Messages newMess = new Messages();
+                newMess.mes_id = mesg.Mes_id;
+                newMess.mes_sender = mesg.Mes_sender;
+                newMess.mes_content = mesg.Mes_content;
+                newMess.mes_status = mesg.Mes_status;
+                messages.Add(newMess);
+            }
+
+            return messages;
+        }
+
+        public static async Task saveMessage(Messages message)
+        {
+            CScore.DataLayer.Tables.InboxL DbMessage = new InboxL();
+            DbMessage.Mes_id = message.mes_id;
+            DbMessage.Mes_sender = message.mes_sender;
+            DbMessage.Mes_status = message.mes_status;
+            //DbMessage.Mes_time = current time 
+            DbMessage.Mes_content = message.mes_content;
+
+         var results =  await DBuilder._connection.Table<InboxL>().Where(i => i.Mes_id.Equals(message.mes_id)).CountAsync();
+
+            if (results <= 0)
+            {
+                await DBuilder._connection.InsertAsync(DbMessage);
+            }
+            else
+                await DBuilder._connection.UpdateAsync(DbMessage);
+
+        }
     }
 }
