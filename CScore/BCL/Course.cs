30089b3cbaca248872ff462d537a4a13ba424321
mtellesy <m.al-tellesy@uot.edu.ly>
2016-12-06 15:16:49 +0200

    Update

--- CScore/BCL/Course.cs
+++ CScore/BCL/Course.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CScore.DataLayer;
 
 namespace CScore.BCL
 {
@@ -12,10 +13,33 @@ namespace CScore.BCL
         public String cou_nameAR { get; set; }
         public String cou_nameEN { get; set; }
         public int Ter_id { get; set; }
-        public List<Schedule> schedule { get; set; }
         public int cou_credits { get; set; }
-       
-       
+        public List<Schedule> schedule { get; set; }
+        
+
+        public static async Task<Course> getCourse(String courseID)
+        {
+            Course course = new Course();
+            course = await DAL.CourseD.getCourse(courseID,User.use_type);
+            return course;
+        }
+
+        public static async Task<List<Course>> getUserCoursesSchedule()
+        {
+            List<Course> courses = new List<Course>();
+
+            if(UpdateBox.CheckForInternetConnection() == true)
+            {
+                //SAL stuff
+                //save what you got from sal anyway
+            }
+
+            courses = await DAL.CourseD.getUserCoursesSchedule(Semester.current_term);
+
+            return courses;
+            
+        }
+
 
     }
 }
