67bd69320c0c7bfdf7f8471f8b7e33c660bce328
mtellesy <m.al-tellesy@uot.edu.ly>
2016-12-10 17:23:24 +0200

    Playing with AuthenticatorS

--- CScore/SAL/AuthenticatorS.cs
+++ CScore/SAL/AuthenticatorS.cs
@@ -5,11 +5,21 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Net;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using CScore.BCL;
 using System.Net.Http;
 
 namespace CScore.SAL
 {
+    public class RootObject
+    {
+        public int dstOffset { get; set; }
+        public int rawOffset { get; set; }
+        public string status { get; set; }
+        public string timeZoneId { get; set; }
+        public string timeZoneName { get; set; }
+    }
+
     public static class AuthenticatorS
     { 
         public static String domain; // this must be set from Application layer
@@ -18,27 +28,68 @@ namespace CScore.SAL
 
 
 
-        public static async Task<Object> sendRequest(String path, Object requestObject, String requestType)
+        public static async Task<String> sendRequest(String path, Object requestObject, String requestType)
         {
+            response = new List<String>();
             //String fullPath = domain + path;
             String fullPath = "https://maps.googleapis.com/maps/api/timezone/json?location=38.908133,-77.047119&timestamp=1458000000&key=AIzaSyAoVToLOAWOxSYTe_3SSHqWB3vjFXYWUtA";
-            //HttpWebRequest request = (HttpWebRequest)WebRequest.CreateHttp(fullPath);
+            
             HttpClient request = new HttpClient();
             request.Timeout = TimeSpan.FromMilliseconds(5000);
-            
+            HttpResponseMessage httpResponse;
+            HttpContent content;
+            String jsonString;
+            if (requestObject != null)
+            {
+                var json = new JObject(new JProperty("json-key-name", requestObject)).ToString();
+                content = new StringContent(json);
+            }
+            else content = null;
+
             if (await UpdateBox.CheckForInternetConnection() == true)
             {
                 try
                   {
-                    HttpResponseMessage httpResponse = await request.GetAsync(fullPath);
-                      var code = httpResponse.StatusCode;
-                  
-                      if ((int)code == 200)
-                      {
-                          return "okay" + code.ToString();
-                      }
-                      else
-                      { return "not okay"; }
+                    switch(requestType)
+                    {
+                        case "Post":
+                        case "POST":
+                        case "post":
+                            httpResponse = await request.PostAsync(fullPath,content);
+                            return null;
+                            break;
+                        case "Put":
+                        case "PUT":
+                        case "put":
+                            return null;
+                            break;
+                            // GET 
+                        case "Get":
+                        case "GET":
+                        case "get":
+
+                            httpResponse = await request.GetAsync(fullPath);
+                            httpResponse.EnsureSuccessStatusCode();
+                            jsonString = await httpResponse.Content.ReadAsStringAsync();
+                          //  returnedObject =  JsonConvert.DeserializeObject<RootObject>(body);
+                            response.Add(httpResponse.StatusCode.ToString());
+                            response.Add(jsonString);
+                            return jsonString;
+                            break;
+                            
+                        case "Delete":
+                        case "DELETE":
+                        case "delete":
+                            return null;
+                            break;
+                        default:
+                            return null;
+                            break;
+
+                            
+
+                    }
+                 
                   }
                   catch (Exception ex)
                 { return "Error"; }
